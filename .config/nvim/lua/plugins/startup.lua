18ed3423a4e22d494cce04effbc1e7ce5c745144
obi-3 <ct4kuto@gmail.com>
2023-02-23 17:25:33 +0900

    Update

--- .config/nvim/lua/plugins/startup.lua
+++ .config/nvim/lua/plugins/startup.lua
@@ -7,7 +7,6 @@ return {
     local theta = require 'alpha.themes.theta'
     local dashboard = require("alpha.themes.dashboard")
     local header = theta.header
-    local section_mru = theta.section_mru
     local buttons = {
       type = "group",
       val = {
@@ -23,7 +22,29 @@ return {
       },
       position = "center",
     }
-
+    local cdir = vim.fn.getcwd()
+    local section_mru = {
+      type = "group",
+      val = {
+        {
+          type = "text",
+          val = "Recent files",
+          opts = {
+            hl = "SpecialComment",
+            shrink_margin = false,
+            position = "center",
+          },
+        },
+        { type = "padding", val = 1 },
+        {
+          type = "group",
+          val = function()
+            return { theta.mru(0, cdir) }
+          end,
+          opts = { shrink_margin = false },
+        },
+      },
+    }
     theta.config.layout = {
       { type = "padding", val = 2 },
       header,
