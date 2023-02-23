5fd962e231cc4801daa0ee2270825748f35bfe8d
obi-3 <ct4kuto@gmail.com>
2023-02-23 18:24:18 +0900

    Update

--- install/lib/install-neovim.sh
+++ install/lib/install-neovim.sh
@@ -7,4 +7,6 @@ if [[ $distro == "debian" ]]; then
     sudo add-apt-repository ppa:neovim-ppa/unstable
     sudo apt-get update
     sudo apt-get install neovim
+
+    nvim --headless "+Lazy! sync" +qa
 fi
--- install/lib/install-packages.sh
+++ install/lib/install-packages.sh
@@ -6,5 +6,5 @@ source $(dirname "${BASH_SOURCE[0]:-$0}")/util.sh
 
 distro=$(whichdistro)
 if [[ $distro == "debian" ]]; then
-	checkinstall zsh git tmux bc curl wget unzip fzf ripgrep exa
+	checkinstall zsh git tmux bc curl wget unzip fzf ripgrep
 fi
