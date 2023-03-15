2288b6f4f5b6f37d90d7c9bc3e5c8e17d785d80d
obi-3 <ct4kuto@gmail.com>
2023-03-16 08:46:49 +0900

    nvim: add telescope fzf native

--- .config/nvim/lua/rc/telescope.lua
+++ .config/nvim/lua/rc/telescope.lua
@@ -1,31 +1,38 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	event = "VeryLazy",
+  "nvim-telescope/telescope.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+    "nvim-treesitter/nvim-treesitter",
+    "folke/which-key.nvim",
+  },
+  config = function()
+    local telescope = require("telescope")
+    telescope.setup({
+      defaults = {},
+      extensions = {
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+          -- the default case_mode is "smart_case"
+        }
+      }
+    })
+    telescope.load_extension("fzf")
 
-	dependencies = {
-		-- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		"nvim-tree/nvim-web-devicons",
-		"nvim-treesitter/nvim-treesitter",
-		"folke/which-key.nvim",
-	},
-
-	config = function()
-		local telescope = require("telescope")
-		telescope.setup({
-			defaults = {},
-		})
-		-- telescope.load_extension("fzf")
-
-		local wk = require("which-key")
-		wk.register({
-			["<leader>b"] = { "<cmd>Telescope buffers<cr>", "Open buffer picker" },
-			["<leader>d"] = { "<cmd>Telescope diagnostics<cr>", "Open diagnostic picker" },
-			["<leader>f"] = { "<cmd>Telescope find_files<cr>", "Open file picker" },
-			["<leader>h"] = { "<cmd>Telescope help_tags<cr>", "Open help picker" },
-			["<leader>i"] = { "<cmd>Telescope live_grep<cr>", "Open live grep" },
-			["<leader>o"] = { "<cmd>Telescope oldfiles<cr>", "Open file picker at oldfiles" },
-			["<leader>q"] = { "<cmd>Telescope quickfix<cr>", "Open quickfix picker" },
-			["<leader>s"] = { "<cmd>Telescope treesitter<cr>", "Open symbol picker" },
-		})
-	end,
+    local wk = require("which-key")
+    wk.register({
+      ["<leader>b"] = { "<cmd>Telescope buffers<cr>", "Open buffer picker" },
+      ["<leader>d"] = { "<cmd>Telescope diagnostics<cr>", "Open diagnostic picker" },
+      ["<leader>f"] = { "<cmd>Telescope find_files<cr>", "Open file picker" },
+      ["<leader>h"] = { "<cmd>Telescope help_tags<cr>", "Open help picker" },
+      ["<leader>i"] = { "<cmd>Telescope live_grep<cr>", "Open live grep" },
+      ["<leader>o"] = { "<cmd>Telescope oldfiles<cr>", "Open file picker at oldfiles" },
+      ["<leader>q"] = { "<cmd>Telescope quickfix<cr>", "Open quickfix picker" },
+      ["<leader>s"] = { "<cmd>Telescope treesitter<cr>", "Open symbol picker" },
+    })
+  end,
 }
