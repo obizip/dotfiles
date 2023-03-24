0d4bb0fb70e4ff6c84aac08f661eedde657d5762
obi-3 <ct4kuto@gmail.com>
2023-03-24 12:00:15 +0900

    nvim: update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,7 +16,7 @@ require("options")
 
 require("lazy").setup("rc", {
 	defaults = { lazy = true },
-	checker = { enabled = true },
+	checker = { enabled = false },
 	performance = {
 		cache = {
 			enabled = true,
