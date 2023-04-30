a352680488dc343bf0641a1dc247581a0b48b985
obi-3 <ct4kuto@gmail.com>
2023-04-30 14:06:34 +0900

    setup: update

--- setup/newsetup.sh
+++ setup/newsetup.sh
@@ -2,12 +2,6 @@
 
 set -ue
 
-function init() {
-    mkdir -p $HOME/Dev/src
-    git config --global ghq.root "$HOME/Dev/src"
-    git config --global include.path "$HOME/.config/git/gitconfig_shared"
-}
-
 function link() {
     local current_dir=$(dirname "${BASH_SOURCE[0]:-$0}")
     source $current_dir/lib/link-to-homedir.sh
@@ -28,14 +22,20 @@ function install() {
     source $current_dir/lib/install-starship.sh
 }
 
+function git_config() {
+    mkdir -p $HOME/Dev/src
+    git config --global ghq.root "$HOME/Dev/src"
+    git config --global include.path "$HOME/.config/git/gitconfig_shared"
+}
+
 current_dir=$(dirname "${BASH_SOURCE[0]:-$0}")
 source $current_dir/lib/util.sh
-
-print_info initialize...
-init
 
 print_info linking...
 link
 
 print_info installing...
 install
+
+print_info config git...
+git_config
