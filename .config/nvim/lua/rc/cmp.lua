cdc61762d62d04a2805f0189ffdafb77dd6410bd
obi-3 <ct4kuto@gmail.com>
2023-05-16 21:28:21 +0900

    nvim: update completion

--- .config/nvim/lua/rc/cmp.lua
+++ .config/nvim/lua/rc/cmp.lua
@@ -1,6 +1,6 @@
 local M = {
   "hrsh7th/nvim-cmp",
-  event = {"InsertEnter", "CmdlineEnter"},
+  event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
     "L3MON4D3/LuaSnip",
     "hrsh7th/cmp-buffer",
@@ -22,9 +22,15 @@ function M.config()
   local cmp = require("cmp")
   local luasnip = require('luasnip')
   local lspkind = require("lspkind")
-  local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+  local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  end
 
   cmp.setup({
+    preselect = require('cmp').PreselectMode.None,
     -- disable completion in comments
     enabled = function()
       local context = require("cmp.config.context")
@@ -33,7 +39,8 @@ function M.config()
       if vim.api.nvim_get_mode().mode == "c" then
         return true
       else
-        return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment") and not(buftype == "prompt")
+        return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment") and
+            not (buftype == "prompt")
       end
     end,
     snippet = {
@@ -45,27 +52,40 @@ function M.config()
       -- ["<C-f>"] = cmp.mapping.scroll_docs( -4),
       -- ["<C-b>"] = cmp.mapping.scroll_docs(4),
 
-      ["<Tab>"] = cmp.mapping.select_next_item(),
-      ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          -- they way you will only jump inside the snippet region
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
 
-      ['<C-k>'] = cmp.mapping(function(fallback)
-        if luasnip.jumpable(1) then
-          luasnip.jump(1)
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
         else
           fallback()
         end
-      end, { 'i', 's' }),
-      ['<C-j>'] = cmp.mapping(function(fallback)
-        if luasnip.jumpable( -1) then
-          luasnip.jump( -1)
-        else
-          fallback()
-        end
-      end, { 'i', 's' }),
+      end, { "i", "s" }),
 
       -- ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ["<CR>"] = cmp.mapping(function(fallback)
+        if cmp.visible() and cmp.get_active_entry() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+        else
+          fallback()
+        end
+      end, { "i", "s", "c" }
+      ),
     }),
     sources = cmp.config.sources({
       { name = "luasnip" },
@@ -84,15 +104,9 @@ function M.config()
     },
     formatting = {
       format = lspkind.cmp_format({
-        mode = "symbol", -- show only symbol annotations
-        maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+        mode = "symbol",       -- show only symbol annotations
+        maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
         ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-
-        -- The function below will be called before any actual modifications from lspkind
-        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-        before = function(entry, vim_item)
-          return vim_item
-        end,
       }),
     },
   })
@@ -115,6 +129,7 @@ function M.config()
   })
 
   -- Insert '(' after select function or method
+  local cmp_autopairs = require("nvim-autopairs.completion.cmp")
   cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
 
