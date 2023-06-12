84f8a90ab51fb6f07f021905357cf42eb502e756
obizip <ct4kuto@gmail.com>
2023-06-12 14:18:57 +0900

    update

--- .config/nvim/lua/rc/display.lua
+++ .config/nvim/lua/rc/display.lua
@@ -120,18 +120,27 @@ return {
       -- vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1a1b23 gui=nocombine]]
       -- vim.cmd [[highlight IndentBlanklineIndent2 guibg=#16171F gui=nocombine]]
 
+      vim.opt.list = true
+      vim.opt.listchars:append "eol:↴"
+
       require("indent_blankline").setup {
-        char = "",
-        char_highlight_list = {
-          "IndentBlanklineIndent1",
-          "IndentBlanklineIndent2",
-        },
-        space_char_highlight_list = {
-          "IndentBlanklineIndent1",
-          "IndentBlanklineIndent2",
-        },
-        show_trailing_blankline_indent = false,
+        -- show_end_of_line = true,
+        -- space_char_blankline = " ",
+        show_current_context = true,
+        show_current_context_start = true,
       }
+      -- require("indent_blankline").setup {
+      --   char = "",
+      --   char_highlight_list = {
+      --     "IndentBlanklineIndent1",
+      --     "IndentBlanklineIndent2",
+      --   },
+      --   space_char_highlight_list = {
+      --     "IndentBlanklineIndent1",
+      --     "IndentBlanklineIndent2",
+      --   },
+      --   show_trailing_blankline_indent = false,
+      -- }
     end
   }
 }
--- .config/nvim/lua/rc/util.lua
+++ .config/nvim/lua/rc/util.lua
@@ -164,10 +164,28 @@ return {
     'echasnovski/mini.cursorword',
     version = '*',
     event = "VeryLazy",
-    config = function ()
+    config = function()
       require('mini.cursorword').setup({
         delay = 100,
       })
+
+      _G.cursorword_blocklist = function()
+        local curword = vim.fn.expand('<cword>')
+        local filetype = vim.api.nvim_buf_get_option(0, 'filetype')
+
+        -- Add any disabling global or filetype-specific logic here
+        local blocklist = {}
+        if filetype == 'lua' then
+          blocklist = { 'local', 'require', 'vim'}
+        elseif filetype == 'javascript' then
+          blocklist = { 'import' }
+        end
+
+        vim.b.minicursorword_disable = vim.tbl_contains(blocklist, curword)
+      end
+
+      -- Make sure to add this autocommand *before* calling module's `setup()`.
+      vim.cmd('au CursorMoved * lua _G.cursorword_blocklist()')
     end
 
   },
