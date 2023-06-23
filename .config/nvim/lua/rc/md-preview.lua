30348bb9545a16bbe992e9126adca7c6288bb93b
obi-3 <ct4kuto@gmail.com>
2023-06-24 08:25:39 +0900

    nvim: update

--- .config/nvim/lua/rc/md-preview.lua
+++ .config/nvim/lua/rc/md-preview.lua
@@ -1,10 +1,30 @@
 return {
   {
+    -- {
+    --   "iamcco/markdown-preview.nvim",
+    --   ft = "markdown",
+    --   build = ":call mkdp#util#install()",
+    --   -- install manually :call mkpd#util#install()
+    -- },
     {
-      "iamcco/markdown-preview.nvim",
+      'toppair/peek.nvim',
+      build = 'deno task --quiet build:fast',
       ft = "markdown",
-      build = ":call mkdp#util#install()",
-      -- install manually :call mkpd#util#install()
-    },
+      keys = {
+        {
+          "<leader>p",
+          function()
+            local peek = require("peek")
+            if peek.is_open() then
+              peek.close()
+            else
+              peek.open()
+            end
+          end,
+          desc = "Markdown Preview"
+        },
+        opts = { theme = "dark" }
+      }
+    }
   },
 }
