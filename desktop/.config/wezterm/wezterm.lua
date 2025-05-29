a980cbc88f5904ae9ed83e0c2d001aa0f6d732d2
obizip <git@obizip.jp>
2025-05-29 13:47:07 +0900

    add wezterm

---
+++ desktop/.config/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require 'wezterm'
+local keymaps = require 'keymaps'
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font 'CodeNewRoman Nerd Font Mono'
+config.font_size = 14
+
+config.keys = keymaps.keys
+config.key_tables = keymaps.key_tables
+config.mouse_bindings = keymaps.mouse_bindings
+config.disable_default_key_bindings = true
+
+config.hide_tab_bar_if_only_one_tab = true
+
+-- nightly only :(
+-- config.show_close_tab_button_in_tabs = false
+
+return config
