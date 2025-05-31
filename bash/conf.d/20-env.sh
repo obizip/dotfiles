fba2d71eaf83b490e32228e4da2fb4b83afb42d2
obizip <git@obizip.jp>
2025-05-31 11:10:34 +0900

    update bash config

--- bash/conf.d/20-env.sh
+++
@@ -1,5 +0,0 @@
-export PATH="$HOME/.local/bin:$PATH"
-
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-export PAGER=bat
-export EDITOR=nvim
--- bash/conf.d/40-integration.sh
+++
@@ -1,8 +0,0 @@
-# eval "$(mise activate bash)"
-# eval "$(mise activate bash --shims)"
-
-if [ -f ~/.config/bash/plugins/git-completion.bash ]; then
-    source ~/.config/bash/plugins/git-completion.bash
-fi
-
-eval "$(fzf --bash)"
