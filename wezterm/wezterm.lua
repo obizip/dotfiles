2e53f0a596c17df0f1ec7ec49b4f9de3f54fc018
obizip <git@obizip.jp>
2025-06-12 07:08:39 +0900

    update colors

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,6 +10,21 @@ config.keys = keymap.keys
 config.key_tables = keymap.key_tables
 config.mouse_bindings = keymap.mouse_bindings
 
+config.colors = {
+  foreground = '#EEEEEE',
+  background = '#282a34',
+  brights = {
+    'grey',
+    'red',
+    'lime',
+    'yellow',
+    'dodgerblue',
+    'fuchsia',
+    'aqua',
+    'white',
+  },
+}
+
 config.disable_default_key_bindings = true
 config.hide_tab_bar_if_only_one_tab = true
 config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
